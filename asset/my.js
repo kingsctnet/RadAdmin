d01fa6c90d6f2c162c0b74142af35f2a9b19c006
Ratchakorn Nonsiri <kingsctnet@users.noreply.github.com>
2022-05-24 15:34:20 +0700

    Add files via upload

--- asset/my.js
+++ asset/my.js
@@ -1,1 +1,33 @@
+//Delete User
+$(document).on('click','.delUser',function(){
+  var isOk = confirm("Delete this user?");
+  if(isOk){
+    var user = $(this).attr('data-id');
+    $.ajax({
+    method: "POST",
+    url: "function/ajax_functions.php",
+    data: { id : user, action: "delUser" },
+    success: function(data){
+      $("#data_"+user).remove();
+        console.log(data);
+      }
+    })
+  }
+});
 
+//Delete Group
+$(document).on('click','.delGroup',function(){
+  var isOk = confirm("Delete this group?");
+  if(isOk){
+    var group = $(this).attr('data-id');
+    $.ajax({
+    method: "POST",
+    url: "function/ajax_functions.php",
+    data: { id : group, action: "delGroup" },
+    success: function(data){
+      $("#data_"+group).remove();
+        console.log(data);
+      }
+    })
+  }
+});
