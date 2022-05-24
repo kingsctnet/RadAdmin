86e73942fd57bb5fe55620a1c5e785a103d7d94b
Ratchakorn Nonsiri <kingsctnet@users.noreply.github.com>
2022-05-24 15:29:34 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="asset/jquery/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="asset/materialize/js/materialize.min.js"></script>
+      <script type="text/javascript" src="asset/materialize/js/my.js"></script>
+      <script type="text/javascript" src="asset/datatables/jquery.dataTables.js"></script>
+      <script type="text/javascript" src="asset/my.js"></script>
+      <script>
+        $(document).ready( function () {
+          $('#table_id').DataTable();
+        } );
+      </script>
+    </body>
+</html>
